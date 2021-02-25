f99d2133a983608095b8bd62746ab5261d5e6c00
Aleksei Zhuravlev <aa_zhur@mail.ru>
2021-02-25 19:20:32 +0000

    Add sol

--- src/lexa.cpp
+++ src/lexa.cpp
@@ -18,7 +18,34 @@ using namespace std;
 
 struct MySolver : public Context {
     void Solve() {
-        // Solution goes here
+        std::vector<int> streetCounts(StreetN, 0);
+        for (int i = 0; i < CarN; ++i) {
+            for (int j = 0; j < Path[i].size(); ++j) {
+                ++streetCounts[Path[i][j]];
+            }
+        }
+        Solution.resize(IntersectionN);
+        for (int i = 0; i < IntersectionN; i++) {
+            Solution[i].IncomingStreetDuration.resize(IncomingStreets[i].size());
+            int totalStreetCount = 0;
+            for (int j = 0; j < IncomingStreets[i].size(); j++) {
+                int street = IncomingStreets[i][j];
+                totalStreetCount += streetCounts[street];
+            }
+            if (totalStreetCount == 0) {
+                for (int j = 0; j < IncomingStreets[i].size(); j++) {
+                    Solution[i].IncomingStreetDuration[j] = 1;
+                }
+                continue;
+            }
+
+            int curCount = IncomingStreets[i].size();
+            for (int j = 0; j < IncomingStreets[i].size(); j++) {
+                int street = IncomingStreets[i][j];
+                double weight = streetCounts[street] / totalStreetCount;
+                Solution[i].IncomingStreetDuration[j] = streetCounts[street];
+            }
+        }
     }
 };
 
